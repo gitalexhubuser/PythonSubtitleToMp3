3445f1beeeb617413369e2ef0a5c97651e108929
ilovetiktiok <alexdubovyck@yandex.ru>
2023-05-28 22:27:59 +0300

    Гайд по Виртуальному окружению

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
-import winsound
 from gtts import gTTS
-import codecs
-import re
+import re, codecs, winsound
 
-# Открыть файл с текстом и прочитать его содержимое
-# with open('1. Getting started with Blender 3.1.srt', 'r') as file:
-#     text = file.read()
+FILE = '1. Getting started with Blender 3.1.srt'
+# FILE = '1. Getting started with Blender 3.2.srt'
 
-with codecs.open('1. Getting started with Blender 3.2.srt', 'r', encoding='utf-8') as f:
-    text = f.read()
+def SubtitleToMp3(fileName):
+    # Открыть файл с текстом и прочитать его содержимое
+    with codecs.open(fileName, 'r', encoding='utf-8') as f:
+        text = f.read()
 
-    # Удаление номеров строк и временных меток.
-    text2 = re.sub(r'\d+\n\d{2}:\d{2}:\d{2},\d+ --> \d{2}:\d{2}:\d{2},\d+\n', '', text)
+        # Удаление номеров строк и временных меток.
+        text2 = re.sub(r'\d+\n\d{2}:\d{2}:\d{2},\d+ --> \d{2}:\d{2}:\d{2},\d+\n', '', text)
 
-# Инициализировать объект gTTS с текстом и языком
-tts = gTTS(text=text2, lang='ru')
+    # Инициализировать объект gTTS с текстом и языком
+    tts = gTTS(text = text2, lang = 'ru')
 
-# Сохранить аудиофайл
-tts.save("output.mp3")
-winsound.Beep(2500, 500)
+    # Сохранить аудиофайл
+    tts.save("output.mp3")
+
+    # Подать звуковой сигнал, когда всё будет готово!
+    winsound.Beep(2500, 500)
+
+
+if __name__ == "__main__":
+    SubtitleToMp3(FILE)
